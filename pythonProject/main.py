051c742df168d3b4abd27e1ec62166e668fda0e6
Ashhad06 <ashhadchaghtai07@gmail.com>
2023-06-25 21:41:42 +0500

    first commit

---
+++ pythonProject/main.py
@@ -0,0 +1,6 @@
+Tbl = int(input("Enter Your table number :"))
+n1 = int(input("Enter Your table start number :"))
+n2 = int(input("Enter Your table End number :"))
+
+for i in range(n1,n2):
+    print(Tbl, "x", i, "=", Tbl*i)
