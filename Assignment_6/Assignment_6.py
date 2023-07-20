fb28824a5dd7c4842197b2308644a7bb05019380
Ashhad06 <129995285+Ashhad06@users.noreply.github.com>
2023-07-20 22:20:21 +0500

    Add files via upload

--- Assignment_6/Assignment_6.py
+++ Assignment_6/Assignment_6.py
@@ -11,18 +11,28 @@ def divide(x,y):
     return x/y
 
 def calculator():
-    A=input("Enter what do you want to do? (+,-,*,/) : ")
-    Num1 = int(input("Enter Your first number :"))
-    Num2 = int(input("Enter Your second number"))
-    if A== "+":
-        result= add(Num1,Num2)
-    elif A== "-":
-        result= subtract(Num1,Num2)
-    elif A== "*":
-        result= multiply(Num1,Num2)
-    elif A== "/":
-        result= divide(Num1,Num2)
-    else:
-        print("Invalid! Pls try again")
-    print("Your result is :"+ str(result))
+    for i in range(99999):
+        A=input("Enter what do you want to do? (+,-,*,/) : ")
+        if A== "":
+            break
+        Num1 = int(input("Enter Your first number :"))
+        Num2 = int(input("Enter Your second number :"))
+        if A== "+":
+            result= add(Num1,Num2)
+        elif A== "-":
+            result= subtract(Num1,Num2)
+        elif A== "*":
+            result= multiply(Num1,Num2)
+        elif A== "/":
+            result= divide(Num1,Num2)
+        else:
+            print("Invalid! Pls try again")
+        print("Your result is :"+ str(result))
 calculator()
+
+
+
+
+
+
+   
